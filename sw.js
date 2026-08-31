0e396555e43d663e695aa604e9e780a9c00b7c5d
Jake D <jakedryka1@gmail.com>
2026-08-31 19:13:01 +0800

    Add offline cache for SwipeStart

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='swipestart-offline-v1';
+const ASSETS=['./','./index.html','./index-v2.html','./styles-v2.css','./ideas-v2.js','./logic-v2.js'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index-v2.html'))))});
