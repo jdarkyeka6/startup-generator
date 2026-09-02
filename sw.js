cb3d64a285ba5c1cfd80d5d538ff76a440971244
Jake D <jakedryka1@gmail.com>
2026-09-02 20:06:10 +0800

    Cache swipe interaction enhancements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='swipestart-offline-v2';
-const ASSETS=['./','./index.html','./index-v2.html','./styles-v2.css','./crisp.css','./ideas-v2.js','./logic-v2.js'];
+const CACHE='swipestart-offline-v3';
+const ASSETS=['./','./index.html','./index-v2.html','./styles-v2.css','./crisp.css','./ideas-v2.js','./logic-v2.js','./swipe-enhancements.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match('./index-v2.html'))))});
